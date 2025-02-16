c6e74b2c99679c4db91ae120e3d8098b4bdf2567
jayjeong8 <zuni4242@gmail.com>
2025-02-16 13:10:34 +0900

    feat: 올바른 adminKey가 있을 때만 admin 페이지에 접근 가능하도록 개선

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,29 @@
+import { redirect } from "next/navigation";
 import { getTeamsByNameAsc } from "@/app/actions";
 import ScoreUpdater from "@/app/admin/_components/ScoreUpdater";
 
-export default async function Admin() {
+export default async function Admin({
+  searchParams,
+}: {
+  searchParams: Promise<{ adminKey: string }>;
+}) {
+  const { adminKey } = await searchParams;
+
+  const validateAdminKey = (adminKey?: string) => {
+    if (!adminKey) return false;
+
+    const validKey = process.env.ADMIN_KEY;
+    if (!validKey) return false;
+
+    return adminKey === validKey;
+  };
+
+  const isValidKey = validateAdminKey(adminKey);
+
+  if (!isValidKey) {
+    redirect("/");
+  }
+
   const teams = await getTeamsByNameAsc();
 
   return (
