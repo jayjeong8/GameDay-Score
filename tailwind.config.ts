6d6aa37dc1d2c44e2e572fd5918abea979ff975b
jayjeong8 <zuni4242@gmail.com>
2025-02-16 15:10:53 +0900

    fix: tailwind 잘못된 설정 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,12 @@
 import type { Config } from "tailwindcss";
 
 export default {
-    darkMode: ["class"],
-    content: [
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/_components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-
-  	}
+    extend: {},
   },
-  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
